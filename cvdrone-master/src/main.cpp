eaa42ca6276fc605c67ac2f129067d832292b84c
LiHeng Hu <huliheng2011@163.com>
2014-06-19 22:18:05 +0800

    ROS-final_project-A_QR-cvdrone-master
    ROS-final_project-A_QR-cvdrone-master

---
+++ cvdrone-master/src/main.cpp
@@ -0,0 +1,16 @@
+#include "ardrone/ardrone.h"
+#include "functionalities/functionalities.h"
+
+// --------------------------------------------------------------------------
+// main(Number of arguments, Argument values)
+// Description  : This is the entry point of the program.
+// Return value : SUCCESS:0  ERROR:-1
+// --------------------------------------------------------------------------
+int main(int argc, char **argv)
+{
+  // Activate the program of locating a colored object
+  int exit_code = LocateColoredObject();
+
+  // Program exit
+  return exit_code;
+}
