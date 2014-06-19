1c7e4074bacc3abd1decad58851203f2cd002204
LiHeng Hu <huliheng2011@163.com>
2014-06-19 22:16:39 +0800

    ROS-final_project-A_QR-publisher
    ROS-final_project-A_QR-publisher

---
+++ publisher/GetRedBoxPosition.cpp
@@ -0,0 +1,14 @@
+float getPositionX() {
+    float x = 1.0;
+    return x;
+}
+
+float getPositionY() {
+    float y = 1.0;
+    return y;
+}
+
+float getReady() {
+    float ready = 0;
+    return ready;
+}
